eea4e8578c7e74f2e0b9c7456d6b70376d7da6b5
kimc <tjdtlr12349@gmail.com>
2024-11-04 22:04:41 +0900

    queryDsl apt 의존성 추가 (QClass 를 위함)

--- src/main/java/com/travel/travelplan/repository/FriendRepositoryCustomImpl.java
+++ src/main/java/com/travel/travelplan/repository/FriendRepositoryCustomImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.travel.travelplan.entity.User;
 
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 @Repository
 @RequiredArgsConstructor
 public class FriendRepositoryCustomImpl implements FriendRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
 
     public List<User> findFriendListByUserId(Integer userId) {
         return null;
