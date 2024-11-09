0a80d52ea93f76ed83942ff5c7552750fc2f252e
Mindundun <pmk10077@gmail.com>
2024-11-09 15:27:53 +0900

    Todolist저장되게했따우

--- src/main/java/com/travel/travelplan/controller/TodoListController.java
+++ src/main/java/com/travel/travelplan/controller/TodoListController.java
@@ -3,16 +3,17 @@ package com.travel.travelplan.controller;
 import java.time.LocalDate;
 import java.util.List;
 
-import org.springframework.stereotype.Controller; // 추가
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
+import org.springframework.validation.BindingResult; // 추가
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.travel.travelplan.entity.Todo;
-import com.travel.travelplan.service.TodoListService;
+import com.travel.travelplan.repository.TodoListRepository;
 
 import jakarta.validation.Valid;
 
@@ -20,72 +21,94 @@ import jakarta.validation.Valid;
 @SessionAttributes("name")
 public class TodoListController {
 
-    private final TodoListService todolistService; // final로 변경하여 불변성을 보장
+    public TodoListController(TodoListRepository todolistRepository) {
+		super();
+		this.todolistRepository = todolistRepository; //이렇게 하면 자동적으로 결합(생성자)
+	}
+		
+	//DB 테이블과 소통하기 위한 선언. -> 생성자 인수로 추가
+	private TodoListRepository todolistRepository;
 
-    public TodoListController(TodoListService todolistService) {
-        this.todolistService = todolistService;
-    }
+	// /todolist
+	@RequestMapping("todolist")
+	public String listAllTodos(ModelMap model) {
+		String username = getLoggedInUsername(model);
+		
+		List<Todo> todos = todolistRepository.findByUsername(username);
+		model.addAttribute("todos",todos);
+		
+		return "todolist";
+	}
 
     // /todolist
-    @RequestMapping("todolist") // 요청 URL
-    public String showTodoList(ModelMap model) {
-        String username = getLoggedInUsername(model); // 로그인한 사용자 이름 가져오기
-        List<Todo> todos = todolistService.findByUsername(username); // 사용자에 대한 Todo 리스트 가져오기
-        model.addAttribute("todos", todos); // 모델에 todos 추가
-        return "todolist"; // todolist.html 파일 반환
-    }
+    // @RequestMapping("todolist") // 요청 URL
+    // public String showTodoList(ModelMap model) {
+    //     String username = getLoggedInUsername(model); // 로그인한 사용자 이름 가져오기
+    //     List<Todo> todos = todolistService.findByUsername(username); // 사용자에 대한 Todo 리스트 가져오기
+    //     model.addAttribute("todos", todos); // 모델에 todos 추가
+    //     return "todolist"; // todolist.html 파일 반환
+    // }
 
-    // GET
-    @RequestMapping(value = "add-todo", method = RequestMethod.GET)
-    public String showNewTodoPage(ModelMap model) {
-        String username = getLoggedInUsername(model);
-        Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
-        model.put("todo", todo);
-        return "todo"; // todo.html 파일 반환
-    }
-
-    // POST
-    @RequestMapping(value = "add-todo", method = RequestMethod.POST)
-    public String addNewTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
-        if (result.hasErrors()) {
-            return "todo"; // 오류가 있을 경우 todo.html 파일 반환
-        }
-
-        String username = getLoggedInUsername(model);
-        todolistService.addTodo(username, todo.getDescription(), todo.getTargetDate(), false);
-
-        return "redirect:todolist"; // 완료 후 todolist로 리다이렉트
-    }
-
-    @RequestMapping("delete-todo")
-    public String deleteTodo(@RequestParam int id) {
-        todolistService.deleteById(id);
-        return "redirect:todolist"; // 삭제 후 todolist로 리다이렉트
-    }
-
-    @RequestMapping(value = "update-todo", method = RequestMethod.GET)
-    public String showUpdateTodo(@RequestParam int id, ModelMap model) {
-        Todo todo = todolistService.findByid(id);
-        model.addAttribute("todo", todo); // 모델에 todo 추가
-        return "todo"; // todo.html 파일 반환
-    }
-
-    @RequestMapping(value = "update-todo", method = RequestMethod.POST)
-    public String updateTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
-        if (result.hasErrors()) {
-            return "todo"; // 오류가 있을 경우 todo.html 파일 반환
-        }
-
-        String username = getLoggedInUsername(model);
-        todo.setUsername(username);
-        todolistService.updateTodo(todo);
-
-        return "redirect:todolist"; // 완료 후 todolist로 리다이렉트
-    }
+    //GET
+	@RequestMapping(value="add-todo", method=RequestMethod.GET)
+	public String showNewTodoPage(ModelMap model) {
+		String username = getLoggedInUsername(model);
+		Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
+		model.put("todo", todo);
+		return "todo";
+	}
+	
+	//POST
+	//@RequestParam String description을 사용하지 않고 Todo의 Description을 가져오도록
+	@RequestMapping(value="add-todo", method=RequestMethod.POST)
+	public String addNewTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "todo";
+		}
+		
+		String username = getLoggedInUsername(model);
+		todo.setUsername(username);//Todo Bean 넣기
+		todolistRepository.save(todo);//왜냐 todoRepository는 모든 속성을 받는 메서드가 없기 때문
+		
+		//todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), todo.isDone());
+		
+		return "redirect:todolist";
+	}
+	
+	@RequestMapping("delete-todo")
+	public String deleteTodo(@RequestParam int id) {
+		todolistRepository.deleteById(id);
+		//todoService.deleteById(id);
+		return "redirect:todolist";
+	}
+	
+	@RequestMapping(value="update-todo", method=RequestMethod.GET)
+	public String showUpdateTodo(@RequestParam int id,ModelMap model) {
+		Todo todo = todolistRepository.findById(id).get();
+		model.addAttribute("todo", todo);//model에 todo추가
+		return "todo";
+	}
+	
+	@RequestMapping(value="update-todo", method=RequestMethod.POST)
+	public String updateTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "todo";
+		}
+		
+		String username = getLoggedInUsername(model);
+		todo.setUsername(username);
+		todolistRepository.save(todo);
+		//todoService.updateTodo(todo);
+		
+		return "redirect:todolist";
+	}
 
     private String getLoggedInUsername(ModelMap model) {
-        return (String) model.get("name"); // 로그인한 사용자 이름 반환
-    }
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return username;
+	}
 
     @RequestMapping(value = "/todomain", method = RequestMethod.GET)
     public String todomain() {
--- src/main/java/com/travel/travelplan/service/TodoListService.java
+++ src/main/java/com/travel/travelplan/service/TodoListService.java
@@ -1,6 +1,5 @@
 package com.travel.travelplan.service;
 
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,36 +13,47 @@ import jakarta.validation.Valid;
 
 @Service
 public class TodoListService {
-	private static final List<Todo> todos = new ArrayList<>();
-	
-	private static int todosCount = 0;
-	
-	public List<Todo> findByUsername(String username){
-		Predicate<? super Todo> predicate = todo -> todo.getUsername().equalsIgnoreCase(username);
-		return todos.stream().filter(predicate).toList();
-	}
-	
-	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
-		Todo todo = new Todo(++todosCount, username, description, targetDate, done);
-		todos.add(todo);
-	}
-	
-	public void deleteById(int id) {
-		//삭제할 때는 todo.getId() == id
-		//Lamda : todo -> todo.getId() == id : 모든 행에 있는 Bean, 즉 Todo에 있는 모든 Bean에 대해 이 조건 실행
-		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
-		todos.removeIf(predicate);
-	}
+    private static final List<Todo> todos = new ArrayList<>();
+    
+    private static int todosCount = 0;
 
-	public Todo findByid(int id) {
-		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
-		Todo todo = todos.stream().filter(predicate).findFirst().get(); // todos 스트림 생성 후 filter로 predicate를 사용해서 필터링 및 첫번째 가져오기 
-		return todo;
-	}
+    public List<Todo> findByUsername(String username) {
+        Predicate<? super Todo> predicate = todo -> todo.getUsername().equalsIgnoreCase(username);
+        return todos.stream().filter(predicate).toList();
+    }
 
-	public void updateTodo(@Valid Todo todo) {
-		deleteById(todo.getId()); // 삭제 후
-		todos.add(todo);	//추가 하는 방식으로 update처리		
-	}
+    public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+        // 로그 추가: addTodo 메서드 호출 시, 전달된 인자 출력
+        System.out.println("Adding new Todo:");
+        System.out.println("Username: " + username);
+        System.out.println("Description: " + description);
+        System.out.println("Target Date: " + targetDate);
+        System.out.println("Done Status: " + done);
+
+        // 새로운 Todo 객체 생성
+        Todo todo = new Todo(++todosCount, username, description, targetDate, done);
+        
+        // Todo 리스트에 추가
+        todos.add(todo);
+        
+        // Todo가 리스트에 추가되었음을 로그로 출력
+        System.out.println("Todo added: " + todo);
+    }
+
+    public void deleteById(int id) {
+        // 삭제할 때는 todo.getId() == id
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        todos.removeIf(predicate);
+    }
+
+    public Todo findByid(int id) {
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        Todo todo = todos.stream().filter(predicate).findFirst().get();
+        return todo;
+    }
+
+    public void updateTodo(@Valid Todo todo) {
+        deleteById(todo.getId());  // 삭제 후
+        todos.add(todo);  // 추가하는 방식으로 update 처리
+    }
 }
-
